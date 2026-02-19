4d8010e91c19af4d0552fe53d2708486dc8d5bb3
qwerty268 <lev-demchenko@bk.ru>
2026-02-19 21:41:54 +0300

    feat: test

--- main_app/internal/chats/repository/repository_integration_test.go
+++ main_app/internal/chats/repository/repository_integration_test.go
@@ -1,15 +1,14 @@
-//go:build integration
-// +build integration
-
 package repository
 
 import (
 	"context"
+	"database/sql"
 	"testing"
 
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/suite"
 
+	model "github.com/go-park-mail-ru/2024_2_EaglesDesigner/main_app/internal/chats/models"
 	testingBoilerplate "github.com/go-park-mail-ru/2024_2_EaglesDesigner/main_app/internal/testing_boilerplate"
 )
 
@@ -34,16 +33,10 @@ func (s *RepoSuite) TearDownTest() {
 	_, err = s.DBSuite.DB.Exec("DELETE FROM chat")
 	s.Require().NoError(err)
 
-	_, err = s.DBSuite.DB.Exec("DELETE FROM chat_type")
-	s.Require().NoError(err)
-
 	_, err = s.DBSuite.DB.Exec("DELETE FROM contact")
 	s.Require().NoError(err)
 
 	_, err = s.DBSuite.DB.Exec("DELETE FROM public.user")
-	s.Require().NoError(err)
-
-	_, err = s.DBSuite.DB.Exec("DELETE FROM user_role")
 	s.Require().NoError(err)
 }
 
@@ -94,3 +87,82 @@ func (s *RepoSuite) TestStorage_GetStores() {
 }
 
 // лев
+
+func (s *RepoSuite) TestStorageSuite_GetCountOfUsersInChat_Success() {
+	repo, err := NewChatRepository(s.DB)
+	s.Require().NoError(err)
+
+	chat := model.Chat{
+		ChatId:            uuid.New(),
+		ChatName:          "test",
+		ChatType:          "group",
+		SendNotifications: true,
+	}
+
+	err = repo.CreateNewChat(context.Background(), chat)
+	s.Require().NoError(err)
+	count, err := repo.GetCountOfUsersInChat(context.Background(), chat.ChatId)
+	s.Require().NoError(err)
+	s.Require().Equal(0, count)
+}
+
+func (s *RepoSuite) TestStorageSuite_GetChatById_NoRows() {
+	repo, err := NewChatRepository(s.DB)
+	s.Require().NoError(err)
+
+	chat, err := repo.GetChatById(context.Background(), uuid.New())
+
+	s.Require().Equal(model.Chat{}, chat)
+	s.Require().ErrorIs(err, sql.ErrNoRows) // Надо бы возвращать ошибку.
+}
+
+func (s *RepoSuite) TestStorageSuite_GetChatById_Success() {
+	repo, err := NewChatRepository(s.DB)
+	s.Require().NoError(err)
+
+	expectedChat := model.Chat{
+		ChatId:   uuid.New(),
+		ChatName: "test",
+		ChatType: "group",
+	}
+
+	err = repo.CreateNewChat(context.Background(), expectedChat)
+	s.Require().NoError(err)
+
+	chat, err := repo.GetChatById(context.Background(), expectedChat.ChatId)
+
+	s.Require().NoError(err)
+	s.Require().Equal(expectedChat, chat)
+}
+
+func (s *RepoSuite) TestStorageSuite_GetNameAndAvatar_NoRows() {
+	repo, err := NewChatRepository(s.DB)
+	s.Require().NoError(err)
+
+	name, avatar, err := repo.GetNameAndAvatar(context.Background(), uuid.New())
+
+	s.Require().ErrorIs(err, sql.ErrNoRows) // А тут все ок) хотя код такой же. чзх.
+	s.Require().Equal(name, "")
+	s.Require().Equal(avatar, "")
+}
+
+// Тут типо для персонального чата берем аватар, но по названибю функции хуйня.
+func (s *RepoSuite) TestStorageSuite_GetNameAndAvatar_Success() {
+	repo, err := NewChatRepository(s.DB)
+	s.Require().NoError(err)
+
+	expectedChat := model.Chat{
+		ChatId:    uuid.New(),
+		ChatName:  "test",
+		ChatType:  "group",
+		AvatarURL: "test",
+	}
+	err = repo.CreateNewChat(context.Background(), expectedChat)
+	s.Require().NoError(err)
+
+	name, avatar, err := repo.GetNameAndAvatar(context.Background(), expectedChat.ChatId)
+
+	s.Require().NoError(err)
+	s.Require().Equal(expectedChat.ChatName, name)
+	s.Require().Equal(expectedChat.AvatarURL, avatar)
+}
