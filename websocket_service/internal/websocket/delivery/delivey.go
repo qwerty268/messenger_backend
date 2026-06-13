62a79daeedffedd4a87459f18da78ef6901916c8
qwerty268 <lev-demchenko@bk.ru>
2026-06-13 20:51:14 +0300

    delete cors from socket

--- websocket_service/internal/websocket/delivery/delivey.go
+++ websocket_service/internal/websocket/delivery/delivey.go
@@ -16,43 +16,7 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 10048,
-
-	CheckOrigin: func(r *http.Request) bool {
-		allowedOrigins := []string{
-			"http://127.0.0.1:8001",
-			"https://127.0.0.1:8001",
-			"http://localhost:8001",
-			"https://localhost:8001",
-			"http://213.87.152.18:8001",
-			"http://212.233.98.59:8001",
-			"https://213.87.152.18:8001",
-			"http://212.233.98.59:8080",
-			"https://212.233.98.59:8080",
-			"https://patefon.site",
-			"http://localhost",
-			"https://localhost",
-			"https://localhost:8083",
-			"http://localhost:8083",
-			"http://localhost:9090",
-			"https://localhost:9090",
-			"http://127.0.0.1:9090",
-			"https://127.0.0.1:9090",
-			// фронтенд в режиме разработки
-			"http://localhost:3000",
-			"https://localhost:3000",
-			"http://localhost:3001",
-			"https://localhost:3001",
-			"http://127.0.0.1:3000",
-			"http://127.0.0.1:3001",
-		}
-
-		for _, origin := range allowedOrigins {
-			if r.Header.Get("Origin") == origin {
-				return true
-			}
-		}
-		return false
-	},
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
 type Webcosket struct {
